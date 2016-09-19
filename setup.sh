ddd60b7a9a12499bfa478cdbf59531a5e6610595
Kyle Frizzell <Kdfrizale@users.noreply.github.com>
2016-09-18 21:37:31 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,25 +5,15 @@ sudo apt-get update
 echo "Installing dependencies..."
 sudo apt-get install usbmount
 
-
-##get uuid of thumb drive
-##run blkid command , ask user to insert thumb drive, run blkid again ; compare differences to find thumb drive
-##parse result to find uuid
 sudo blkid -s UUID -o value > devicesBefore.info
 read -p "Insert the thumb drive to become a key now. Press Enter to continue.."
 sudo blkid -s UUID -o value > devicesAfter.info
 
 DEVICE_UUID = "$(grep -v -f devicesBefore.info devicesAfter.info)"
 
+sed -i "s/THUMBDRIVEUUID=/THUMBDRIVEUUID=$DEVICE_UUID/g" usbmount
 
-
-##edit usbmount to have uuid in the variable value
-
-
-
-##Override usbmount with edited version
 sudo cp usbmount /usr/share/usbmount
-
 
 ##Possibly Add Check_For_Key.sh to /usr/bin directory
 ##sudo cp check_for_key.sh /usr/local/bin/check_for_key.sh
@@ -31,7 +21,6 @@ sudo cp usbmount /usr/share/usbmount
 ##Add Check_For_Key.sh to autorun on boot
 sudo cp check_for_key.sh /etc/init.d/check_for_key.sh
 sudo update-rc.d check_for_key.sh defaults
-
 
 echo "You must reboot for the changes to take effect."
 read -p "Do you wish to reboot now?" yn
