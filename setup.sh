61f6c4e3928f1ee2d3f41820f84a64f36511feae
Kyle Frizzell <Kdfrizale@users.noreply.github.com>
2017-03-24 14:04:26 -0400

    combined

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-read -p "Make sure the thumb drive is NOT currently plugged in... Do you wish to continue?" yn
+
+read -p "Make sure the thumb drive is not currently plugged in... Do you wish to continue?" yn
     case $yn in
         [Yy]* ) break;;
         [Nn]* ) exit;;
@@ -7,7 +8,7 @@ read -p "Make sure the thumb drive is NOT currently plugged in... Do you wish to
     esac
 
 #check for prevous installation
-if[ -f /usr/share/usbmount]
+if [ -f /usr/share/usbmount ]
 then
     read -p "This will override your previous installation of usbmount. Do you wish to continue?" yn
         case $yn in
@@ -15,6 +16,7 @@ then
             [Nn]* ) exit;;
             * ) echo "Please answer yes or no.";;
         esac
+fi
 
 echo "Updating..."
 sudo apt-get update
@@ -24,20 +26,22 @@ sudo apt-get install usbmount
 sudo mkdir /mnt/usbkey
 
 sudo blkid -s UUID -o value > devicesBefore.info
-read -p "Insert the thumb drive to become a key now. Press Enter to continue.."
+echo "Insert the thumb drive to become a key now. Press Enter to continue.."
+read answer
 sudo blkid -s UUID -o value > devicesAfter.info
 
-DEVICE_UUID = "$(grep -v -f devicesBefore.info devicesAfter.info)"
+DEVICE_UUID=$(grep -v -f devicesBefore.info devicesAfter.info)
+echo $DEVICE_UUID
 
 sed -i "s/THUMBDRIVEUUID=/THUMBDRIVEUUID=$DEVICE_UUID/g" usbmount
 
 sudo cp usbmount /usr/share/usbmount
 
 ##Add Check_For_Key.sh to autorun on boot
-sudo chmod +x check_for_key.sh
-sudo cp check_for_key.sh /etc/init.d/check_for_key.sh
-sudo chmod +x /etc/init.d/check_for_key.sh
-sudo update-rc.d check_for_key.sh defaults
+sudo chmod +x check_for_key
+sudo cp check_for_key.sh /etc/init.d/check_for_key
+sudo chmod +x /etc/init.d/check_for_key
+sudo update-rc.d check_for_key defaults
 
 echo "You must reboot for the changes to take effect."
 read -p "Do you wish to reboot now?" yn
